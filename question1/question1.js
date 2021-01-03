42a54ce72b955ebd437cbfe087d70f747845b860
SarahAlshemari <saraalshemari@outlook.com>
2021-01-03 23:15:38 +0300

    done

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,17 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+function sayHello(name){
+
+    
+    if (name==null)
+    {
+        console.log(`try again`);
+    } 
+    else
+    {
+        console.log(`مرحباً 👋🏻${name}`);
+    }
+   
+}
+
+sayHello(`sarah`);
+sayHello();
